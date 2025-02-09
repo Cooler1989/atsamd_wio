7d63de27bcc574b568384111c1def778a46cb51c
Pawel Pyszko <pyszkomeister@gmail.com>
2025-02-09 22:06:58 +0100

    Implementing draft of SPI device for display

--- boards/wio_terminal/src/display.rs
+++ boards/wio_terminal/src/display.rs
@@ -10,9 +10,11 @@ use atsamd_hal::sercom::spi;
 use atsamd_hal::sercom::{IoSet4, Sercom7};
 use atsamd_hal::time::Hertz;
 use atsamd_hal::typelevel::NoneT;
-use display_interface_spi::{SPIInterface};
 use display_interface::{WriteOnlyDataCommand, DisplayError, DataFormat};
 use ili9341::{DisplaySize240x320, Ili9341, Orientation};
+use atsamd_hal::dmac::DmaController;
+use atsamd_hal::sercom::spi::Spi;
+use atsamd_hal::gpio::Pin;
 
 use super::pins::aliases::*;
 
@@ -46,42 +48,77 @@ pub type LcdSpi = spi::Spi<spi::Config<LcdPads>, spi::Tx>;
 /// Type alias for the ILI9341 LCD display.
 //  pub type LCD = Ili9341<SPIInterface<LcdSpi, LcdDc>, LcdReset>;
 pub type LCD = Ili9341<SpiDeviceImpl, LcdReset>;
+//  pub type SPI = Spi<Config<LcdPads>, spi::Tx>;
 
 pub use ili9341::Scroller;
 
-pub struct SpiDeviceImpl {}
-
 #[derive(Debug)]
 enum TemporaryError {}
+
+pub struct SpiDeviceImpl {
+    spi: LcdSpi,
+    cs: LcdCs, 
+    dc: LcdDc,
+}
 
 impl ErrorType for SpiDeviceImpl {
     type Error = ErrorKind;
 }
 
 impl SpiDeviceImpl {
-    fn new() -> Self {
-        Self {}
+    fn new(
+        spi: LcdSpi,
+        cs: LcdCs, 
+        dc: LcdDc) -> Self {
+        Self {spi, cs, dc}
+    }
+}
+
+struct ChipSelectGuard<'a> {
+    cs: &'a mut LcdCs,
+}
+
+impl<'a> ChipSelectGuard<'a> {
+    pub fn new(cs: &'a mut LcdCs) -> Self {
+        cs.set_low().unwrap(); // Assert CS
+        ChipSelectGuard { cs }
+    }
+}
+
+impl<'a> Drop for ChipSelectGuard<'a> {
+    fn drop(&mut self) {
+        self.cs.set_high().unwrap(); // Deassert CS
     }
 }
 
 impl WriteOnlyDataCommand for SpiDeviceImpl {
     fn send_commands(&mut self, commands: DataFormat<'_>) -> Result<(), DisplayError> {
+        ChipSelectGuard::new(&mut self.cs);
+        self.dc.set_low().unwrap(); // Command mode
         Ok(())
     }
     fn send_data(&mut self, data: DataFormat<'_>) -> Result<(), DisplayError> {
+        ChipSelectGuard::new(&mut self.cs);
+        self.dc.set_high().unwrap(); // Data mode
         Ok(())
     }
 }
 
-impl SpiDevice for SpiDeviceImpl {
-    //  fn write(&mut self, words: &[u8]) -> Result<(), Self::Error> {
-    fn transaction(&mut self, operations: &mut [SpiOperation<'_, u8>]) -> Result<(), Self::Error> {
-        Ok(())
-    }
-}
+//  impl SpiDevice for SpiDeviceImpl {
+//      //  fn write(&mut self, words: &[u8]) -> Result<(), Self::Error> {
+//      fn transaction(&mut self, operations: &mut [SpiOperation<'_, u8>]) -> Result<(), Self::Error> {
+//          Ok(())
+//      }
+//  }
 
 impl Display {
-    pub fn new(miso: LcdMisoReset, mosi: LcdMosiReset, sck: LcdSckReset, cs: LcdCsReset, dc: LcdDcReset, reset: LcdResetReset, backlight: LcdBacklightReset) -> Self {
+    pub fn new(miso: LcdMisoReset, 
+        mosi: LcdMosiReset, 
+        sck: LcdSckReset, 
+        cs: LcdCsReset, 
+        dc: LcdDcReset, 
+        reset: LcdResetReset, 
+        backlight: LcdBacklightReset) -> Self {
         Self {
             miso,
             mosi,
@@ -118,12 +155,29 @@ impl Display {
         let dc = self.dc.into_push_pull_output();
         let reset = self.reset.into_push_pull_output();
 
+        //  let dmac = peripherals.dmac;
+        //  let mut dmac = DmaController::init(dmac, &mut peripherals.pm);
+        //  let channels = dmac.split();
+        //  let chan0 = channels.0.init(PriorityLevel::Lvl0);
+        //  let chan1 = channels.1.init(PriorityLevel::Lvl0);
+
+        //  let mut spi = bsp::spi_master(
+        //      &mut clocks,
+        //      100.kHz(),
+        //      peripherals.sercom2,
+        //      &mut peripherals.mclk,
+        //      sclk,
+        //      mosi,
+        //      miso,
+        //  )
+        //  .with_dma_channels(chan0, chan1);
+
         // Create a SPIInterface over the peripheral, then create the ILI9341 driver
         // using said interface and set its default orientation.
-        let interface = SPIInterface::new(spi, dc);
-        let interface_stub = SpiDeviceImpl::new();
+        //  let interface = SPIInterface::new(spi, dc);
+        let interface_spi = SpiDeviceImpl::new(spi, cs, dc);
         let ili9341 = Ili9341::new(
-            interface_stub,
+            interface_spi,
             reset,
             delay,
             Orientation::LandscapeFlipped,
